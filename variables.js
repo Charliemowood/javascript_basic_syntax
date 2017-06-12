ea329d55e8cad8388ab52ab28b0f7be9fb6fdbad
Charlie Wood <charliemowood@gmail.com>
2017-06-12 14:56:48 +0100

    Add information about variables in JavaScript

---
+++ variables.js
@@ -0,0 +1,19 @@
+
+//  Variables can be any type as int and float, JS is weakly typed
+
+var a = 3;
+var b = 9;
+var c = 3.5;
+
+var newNumber = c * b / a;
+
+// undefined
+var cats;
+// just this by itself would be not defined
+console.log(cats);
+
+// Some things are always false
+
+if(cats){ // this is always false
+  console.log("yay");
+}
