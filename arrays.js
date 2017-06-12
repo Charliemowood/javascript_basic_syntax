e27654c1dce40b0339276929255b3009b4614288
Charlie Wood <charliemowood@gmail.com>
2017-06-12 14:52:25 +0100

    Add information about arrays in JavaScript

---
+++ arrays.js
@@ -0,0 +1,18 @@
+// arrays in JavaScript accept any type
+var cats = ["tabby", "ginger", "grey"]
+
+// the first argument acts as a label
+console.log("Cats", cats);
+
+// set up a new Array like this
+
+var dogs = new Array();
+
+// to add items to an Array in JavaScript
+
+dogs.push("Bulldog");
+dogs.push("German Shepherd");
+
+// to find the length in an array in JavaScript
+
+console.log(dogs.length);
