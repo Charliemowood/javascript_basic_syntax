229b093a87bc84bea61ef78cafe8ea980cd5f699
Charlie Wood <charliemowood@gmail.com>
2017-06-12 14:44:37 +0100

    Add function_play.js

---
+++ function_play.js
@@ -0,0 +1,32 @@
+// function for greeting using first name and surname
+
+function greet(first_name, surname){
+  console.log("Howdy, I am " + first_name + " " + surname + ".");
+}
+greet("Chaz", "The baz");
+
+// Function for multiplication
+
+function multiply(a, b){
+  return a * b;
+}
+
+console.log(multiply(5, 3));
+
+// Function for getting out first element of an array
+
+function firstArray(array){
+  return array[0];
+}
+
+console.log(firstArray(["Garry", "Barry"]));
+
+// Function for listing all name in given as parameters
+
+function nameTaker(){
+  var string = "";
+  for (var i = 0; i < arguments.length; i++){
+    string += console.log(arguments[i]);
+  }
+}
+nameTaker("Gaz", "Chaz", "Baz", "Daz", "Raz", "Saz");
